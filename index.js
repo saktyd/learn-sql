51e92d3e06c0f1cdc1add35ec5c75b1901314d70
Sakti Dewantoro <saktyd@gmail.com>
2019-02-21 16:21:11 +0700

    Delete dumpfilename

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello World')
+require('dotenv').config()
+
+const express = require('express')
+const sqlite = require('sqlite')
+
+const app = express()
+const port = process.env.PORT || 8000
+const dbPromise = sqlite.open(process.env.DB_PATH, { Promise })
+
+app.get('/users', async (req, res) => {
+  const db = await dbPromise
+  const user = await db.all(`SELECT * FROM users`)
+
+  if (user) {
+    res.send({
+      message: 'Get one user',
+      user: user
+    })
+  } else {
+    res.send({
+      message: 'Failed to get one user'
+    })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Express app is listening on localhost:${port}`)
+})
